7ed108c7b407ab73ae8c341d1bf93d3c35c16146
Onur Onat Surmeli <onatsurmeli@gmail.com>
2021-04-01 16:25:44 +0200

    game, user and genre scaffolds, genregames model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :genres
+  resources :games
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210401141342_create_genre_games.rb
@@ -0,0 +1,10 @@
+class CreateGenreGames < ActiveRecord::Migration[6.1]
+  def change
+    create_table :genre_games do |t|
+      t.references :game, null: false, foreign_key: true
+      t.references :genre, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
