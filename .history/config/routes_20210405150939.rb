0d7aa9d4a84bcac4ebde032e2691eb9a06dee7c4
Onur Onat Surmeli <onatsurmeli@gmail.com>
2021-04-05 15:47:42 +0200

    -Views adjusted -Routes adjusted -Session controller created

--- .history/config/routes_20210405150939.rb
+++ .history/config/routes_20210405150939.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :genres
   resources :games
-  resources :users do
-    resources :genres
-    resources :games
-  end
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
