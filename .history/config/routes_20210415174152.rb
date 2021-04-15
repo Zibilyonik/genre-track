f3bb06a84a20ca3a759dcc70096b0c45a5561d4a
Onur Onat Surmeli <onatsurmeli@gmail.com>
2021-04-15 17:45:29 +0200

    -Login page now automatically redirects to user page if user is logged in.

---
+++ .history/config/routes_20210415174152.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  resources :users do
+    resources :genres
+    resources :games
+  end
+  resources :sessions, only: [:create, :new, :destroy]
+  root 'sessions#new'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  post '/games', to: 'games#create'
+  post '/genres', to: 'genres#create'
+  get 'users/external', to: 'games#external'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
