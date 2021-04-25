e6da4a13cc9e03f917c725b0780ef966ad8b9040
Onur Onat Surmeli <onatsurmeli@gmail.com>
2021-04-26 00:47:31 +0200

    -Controller tests added.

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'database_cleaner-active_record'
+  gem 'rails-controller-testing'
   gem 'shoulda-matchers'
   gem 'webdrivers'
 end
---
+++ spec/controllers/user_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+RSpec.describe UsersController, type: :controller do
+  describe 'GET show' do
+    it 'should get show' do
+      user = create(:user)
+      log_in(user)
+      get :show, params: { id: user.id }
+      should render_template('show')
+    end
+  end
+
+  describe 'DELETE destroy' do
+    it 'should delete the game' do
+      user = create(:user)
+      log_in(user)
+      expect do
+        delete :destroy, params: { id: user.id }
+      end.to change(User, :count).by(-1)
+    end
+  end
+
+  describe 'redirect to login' do
+    it 'should redirect guest users' do
+      get :index
+      should redirect_to('/login')
+    end
+  end
+end
