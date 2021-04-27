cc243bcaea82de7f692167d2a315461931e1e753
Onur Onat Surmeli <onatsurmeli@gmail.com>
2021-04-27 16:48:51 +0200

    -Main font changed to "Roboto Condensed". -Multiple minor style changes.

--- app/helpers/genres_helper.rb
+++ app/helpers/genres_helper.rb
@@ -14,9 +14,9 @@ module GenresHelper
     games.each do |x|
       @user = User.find(x.user_id)
       @row << content_tag(:tr) do
-        @table = content_tag(:td, x.name, class: 'main-text text-center')
-        @table << content_tag(:td, x.amount, class: 'main-text text-center')
-        @table << content_tag(:td, @user.name, class: %w[main-text text-center])
+        @table = content_tag(:td, x.name, class: 'main-text text-center align-middle')
+        @table << content_tag(:td, x.amount, class: 'main-text text-center align-middle')
+        @table << content_tag(:td, @user.name, class: %w[main-text text-center align-middle])
       end
     end.join.html_safe
     @row
